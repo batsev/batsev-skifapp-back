bdd172f88288ae22d5c7106d8d10c895cb40b228
Nikita Batsev <nikita.batsev@gmail.com>
2019-10-30 22:52:22 +0300

    initial commit

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get("/api/get", (req, response) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(__dirname, "public"));
   app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
 }
 
